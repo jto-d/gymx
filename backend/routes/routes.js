200b9db98e263b5cd823c19c160397c64f83f79e
jto-d <jodonnell072@gmail.com>
2022-11-09 23:27:47 -0500

    bug

--- backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const { response } = require('express')
-const express = require('express')
-const router = express.Router()
-
-router.post('/signup', (request, response) => {
-    response.send('send')
-})
-
-module.exports = router
